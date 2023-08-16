372b6d3697b824be05e2e24d7c70a265f1a11ec2
basement_mustard <120338237+BasementMustard@users.noreply.github.com>
2023-08-16 01:00:12 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame, sys
+from settings import *
+
+class Level:
+  def __init__(self):
+
+    pygame.display.set_caption('Souls In The Fire')
+
+def create_map(self):
+  for row in WORLD_MAP
+
+
+
+
